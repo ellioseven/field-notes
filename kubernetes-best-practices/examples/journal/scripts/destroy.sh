8c1cd1412b76dc8735f5c96819f2944f0599eb2e
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2022-01-12 20:05:48 +1100

    Remove old content

--- kubernetes-best-practices/examples/journal/scripts/destroy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -u
-
-CLUSTER_NAME=$1
-
-gcloud beta container \
-  clusters delete "${CLUSTER_NAME}"
--- kubernetes-best-practices/index.md
+++
@@ -1,3 +0,0 @@
----
-title: "Kubernetes Best Practices"
----
